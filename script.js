69de33e755d9d712692040e87950c5d78b7bbc91
zeropenguin <zerobertons@yahoo.com>
2022-09-25 11:53:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll('.project__image img');
+const lightbox = document.querySelector('#lightbox');
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    lightbox.classList.add('active');
+    if (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    const img = document.createElement('img');
+    img.src = image.src;
+    lightbox.appendChild(img);
+  })
+});
+
+lightbox.addEventListener('click', () => lightbox.classList.remove('active'));
